c7026f76f60d2c44efb54e79765b47af31058bcc
ImaaValenzuela <imanolvalenzuela03@gmail.com>
2024-08-30 18:38:59 -0300

    Add Menu and its Fragments

--- app/src/main/java/com/example/chat/MainActivity.kt
+++ app/src/main/java/com/example/chat/MainActivity.kt
@@ -5,16 +5,72 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.chat.databinding.ActivityMainBinding
+import com.example.chat.fragments.FragmentChats
+import com.example.chat.fragments.FragmentProfile
+import com.example.chat.fragments.FragmentUsers
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.bottomNV.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.item_profile -> {
+                    seeFragmentProfile()
+                    true
+                }
+                R.id.item_users -> {
+                    seeFragmentUsers()
+                    true
+                }
+                R.id.item_chats -> {
+                    seeFragmentChats()
+                    true
+                }
+                else ->{
+                    false
+                }
+            }
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun seeFragmentProfile() {
+        binding.tvTitle.text = "Perfil"
+
+        val fragment = FragmentProfile()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(binding.fragmentFL.id, fragment, "Fragment Profile")
+        fragmentTransaction.commit()
+    }
+
+    private fun seeFragmentUsers () {
+        binding.tvTitle.text = "Usuarios"
+
+        val fragment = FragmentUsers()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(binding.fragmentFL.id, fragment, "Fragment Users")
+        fragmentTransaction.commit()
+    }
+
+    private fun seeFragmentChats () {
+        binding.tvTitle.text = "Chats"
+
+        val fragment = FragmentChats()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(binding.fragmentFL.id, fragment, "Fragment Chats")
+        fragmentTransaction.commit()
     }
 }
